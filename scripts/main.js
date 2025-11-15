064c9f620bc6166f64b254d2b0981f1ffa1bb833
emiteat <45344055khj@gmail.com>
2025-11-15 23:29:09 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -68,9 +68,9 @@ function initPageScripts(page) {
 
   async function initComponents(){
     await Promise.all([
-      loadFragment('#header', 'pages/components/header.html'),
-      loadFragment('#nav', 'pages/components/nav.html'),
-      loadFragment('#footer', 'pages/components/footer.html'),
+      loadFragment('#header', 'header.html'),
+      loadFragment('#nav', 'nav.html'),
+      loadFragment('#footer', 'footer.html'),
     ]);
     const y = document.getElementById('year');
     if (y) y.textContent = new Date().getFullYear();
