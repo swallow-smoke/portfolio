818ea6aa87688c381034f28d2eaf32ab0b86f33d
emiteat <45344055khj@gmail.com>
2025-11-16 00:03:01 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,9 +15,9 @@
 
   async function initComponents() {
     await Promise.all([
-      loadFragment('#', 'components/header.html'),
-      loadFragment('#',    'components/nav.html'),
-      loadFragment('#', 'components/footer.html'),
+      loadFragment('#', 'pages/components/header.html'),
+      loadFragment('#',    'pages/components/nav.html'),
+      loadFragment('#', 'pages/components/footer.html'),
     ]);
 
     const y = document.getElementById('year');
